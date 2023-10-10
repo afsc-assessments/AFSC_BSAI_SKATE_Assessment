e047907099df8592b4f625928fe10662d46d2752
tribuzio <cindy.tribuzio@noaa.gov>
2023-10-10 15:01:09 -0800

    Added a few bridging models

--- Code/Tier3/2023/M14_2_ss_compare.R
+++ Code/Tier3/2023/M14_2_ss_compare.R
@@ -3,28 +3,19 @@ datapath <- paste0(getwd(), "/Code/Tier3/", AYR, "/Model_Runs")
 # Current model name
 Model_name_old1 <- "M14_2_vold"
 Model_name_old2 <- "M14_2_vbridge"
+Model_name_fixdat <- "M14_2_fix_old"
+Model_name_adddat <- "M14_2_addnew"
 Model_name_new <- "M14_2_update"
 
 #######################################################################################
 ######## Model comparisons
 
 # read model outputs
-model_dir_old1 <- here::here(datapath, Model_name_old1)
-model_run_old1 <- r4ss::SS_output(dir = model_dir_old1,
-                                 verbose = TRUE,
-                                 printstats = TRUE)
+setwd(datapath)
+bridge_out <- SSgetoutput(dirvec = c("M14_2_vold","M14_2_vbridge", "M14_2_fix_old", 'M14_2_addnew', 'M14_2_update'))
+setwd("C:/Users/cindy.Tribuzio/Work/SAFE/Assessments/AFSC_BSAI_SKATE_Assessment")
 
-model_dir_old2 <- here::here(datapath, Model_name_old2)
-model_run_old2 <- r4ss::SS_output(dir = model_dir_old2, #does not work
-                                 verbose = TRUE,
-                                 printstats = TRUE)
-
-model_dir_new <- here::here(datapath, Model_name_new)
-model_run_new <- r4ss::SS_output(dir = model_dir_new, #does not work
-                                  verbose = TRUE,
-                                  printstats = TRUE)
-
-model_comp <- r4ss::SSsummarize(list(model_run_old1, model_run_old2, model_run_new))
+model_comp <- r4ss::SSsummarize(bridge_out)
 
 r4ss::SSplotComparisons(model_comp,
                         print = TRUE,
